f285a9d1dcbebf8fc6f42f8f84e18c919fe0be5b
selena santa cruz <selenasantacruzcrespo@gmail.com>
2024-08-30 02:27:19 -0400

    Creacion y ajute de la arquitectura del proyecto

---
+++ cypress.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // Implementa los listeners de eventos aquí
+      on('task', {
+        log(message) {
+          console.log(message);
+          return null;
+        },
+      });
+    },
+    baseUrl: "https://suite8demo.suiteondemand.com",
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    experimentalStudio: true,
+    defaultCommandTimeout: 15000
+  },
+});
